ba338316254d974221c2b2402ca0fa661de2368d
huangziliang <289043018@qq.com>
2015-08-25 14:50:21 +0800

    add transaction事务管理

--- src/main/java/com/hand/spring_JDBC/StudentJDBCTemplate.java
+++ src/main/java/com/hand/spring_JDBC/StudentJDBCTemplate.java
@@ -4,21 +4,45 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class StudentJDBCTemplate implements StudentDAO {
 
 	 private DataSource dataSource;
 	   private JdbcTemplate jdbcTemplateObject; 
-	   
+	   private PlatformTransactionManager transactionManager;
 	   public void setDataSource(DataSource dataSource) {
 		      this.dataSource = dataSource;
 		      this.jdbcTemplateObject = new JdbcTemplate(dataSource);
 		   }
+	   
+	   public void setTransactionManager(
+			      PlatformTransactionManager transactionManager) {
+			      this.transactionManager = transactionManager;
+			   }
+	   
 	   public void create(String name, Integer age) {
-		      String SQL = "insert into Student (name, age) values (?, ?)";     
-		      jdbcTemplateObject.update( SQL, name, age);
-		      System.out.println("Created Record Name = " + name + " Age = " + age);
+		   TransactionDefinition def = new DefaultTransactionDefinition();
+		      TransactionStatus status = transactionManager.getTransaction(def);
+		      try {
+		    	  String SQL = "insert into Student (name, age) values (?, ?)";     
+			      jdbcTemplateObject.update( SQL, name, age);
+			      System.out.println("Created Record Name = " + name + " Age = " + age);
+			      
+			      transactionManager.commit(status);
+				
+			} catch (DataAccessException  e) {
+				System.out.println("Error in creating record, rolling back");
+		         transactionManager.rollback(status);
+		         throw e;
+			}
+		   
+		     
 		      return;
 		   }
 		   public Student getStudent(Integer id) {
